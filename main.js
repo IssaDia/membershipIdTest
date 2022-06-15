253cb208e83683ec5e57734abcabb3410cc0eaf4
IssaDia <44264590+IssaDia@users.noreply.github.com>
2022-06-15 13:11:33 +0800

    minimum working solution

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
 function createCheckDigit(membershipId) {
-  // while (membershipId.length > 1) {
-  //   sumDigits(membershipId);
-  // }
-}
 
-createCheckDigit("125656");
+  // retrieve the sum of the membershipId
+  const result = sumDigits(membershipId);
+
+  // check if the sum of the membershipId is more than two digits then return then return the sum if so
+  if (result.toString().length > 1) {
+     console.log(sumDigits(result.toString()));
+  }
+
+  // return the sum if the total of membership id's is one digit 
+  else {
+
+    console.log(result);
+  }
+    
+
+}
 
 function sumDigits(membershipId) {
-  let membershipIdSum = 0;
 
-  for (let singleMembershipId in membershipId) {
-    membershipIdSum += Number(singleMembershipId);
-    if (membershipIdSum.toString().length === 1) {
-      break;
-    }
+  // put all the separate values in an array
+  const membershipIdArray = membershipId.split("");
+
+  // transform all values from array to numbers
+  const membershipIdArrayNumbered = membershipIdArray.map(Number);
+
+  // initial value from reduce operation representing starting sum operation
+  const initialValue = 0; 
+  
+  // sum all the values and return it
+  const membershipIdReducedSum = membershipIdArrayNumbered.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue
+  );
+   
+      return membershipIdReducedSum;
+    
   }
-//   console.log(membershipIdSum);
-}
+
+createCheckDigit("55555");
+
